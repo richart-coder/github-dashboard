00a3f2dffa2bb7e8cbe62fadd1081aa91da16f7e
richard <104408524+richart-coder@users.noreply.github.com>
2025-05-11 21:55:21 +0800

    feat: 重構通知系統資料庫結構
    - 新增 repository preference 相關 migrations
    - 修改 schema.prisma 支援新的資料結構
    - 新增 userid_repository unique constraint
    - 更新 account provider 設定

---
+++ prisma/migrations/20250511045147_add_userid_repository_unique/migration.sql
@@ -0,0 +1,33 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `provider` on the `Account` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[userId,repository]` on the table `RepositoryPreference` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `providerId` to the `Account` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Account" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "userId" TEXT NOT NULL,
+    "providerType" TEXT NOT NULL,
+    "providerId" TEXT NOT NULL,
+    "providerAccountId" TEXT NOT NULL,
+    "refreshToken" TEXT,
+    "accessToken" TEXT,
+    "accessTokenExpires" DATETIME,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Account" ("accessToken", "accessTokenExpires", "createdAt", "id", "providerAccountId", "providerType", "refreshToken", "updatedAt", "userId") SELECT "accessToken", "accessTokenExpires", "createdAt", "id", "providerAccountId", "providerType", "refreshToken", "updatedAt", "userId" FROM "Account";
+DROP TABLE "Account";
+ALTER TABLE "new_Account" RENAME TO "Account";
+CREATE UNIQUE INDEX "Account_providerId_providerAccountId_key" ON "Account"("providerId", "providerAccountId");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "RepositoryPreference_userId_repository_key" ON "RepositoryPreference"("userId", "repository");
